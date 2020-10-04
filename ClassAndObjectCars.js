a124f378293213955b3a820c777a24f38064ff8c
Desmond Ng Li Teck <70146645+dh8330@users.noreply.github.com>
2020-10-04 23:20:41 +0800

    Add files via upload

---
+++ ClassAndObjectCars.js
@@ -0,0 +1,24 @@
+/** SuperClass
+ * 1. type of cars
+ * -SUv
+ * -mpV
+ * - Hatchback
+ * 
+ * 2 . Object of a type
+ * - merc GLA
+ * - toyota vellfire
+ * - honda jazz
+ * 
+ * 3. To Do
+ *  - define properties and behaviours of each class
+ *  - create classes
+ *  - create inheritance relantiionship between the classses
+ *  - Create objects of the different types of cars
+ */
+
+ 
+
+ Suv.prototype = Newcar();
+ Mpv.prototype = Newcar();
+ Hatchback.prototype = Newcar();
+
